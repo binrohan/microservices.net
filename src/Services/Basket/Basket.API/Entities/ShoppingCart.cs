7ccd409d09cfd5e3d380e7af0aaff9057f3515d0
Bin Rohan <binrohan.cs@gmail.com>
2024-02-20 16:25:16 +0600

    added entity classes

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿namespace Basket.API.Entities;
+
+public class ShoppingCart
+{
+    public string UserName { get; set; } = null!;
+    public List<ShoppingCartItem> Items { get; set; } = [];
+
+    public ShoppingCart()
+    {
+    }
+
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+    public decimal TotalPrice
+    {
+        get
+        {
+            decimal totalprice = 0;
+            foreach (var item in Items)
+            {
+                totalprice += item.Price * item.Quantity;
+            }
+            return totalprice;
+        }
+    }
+}
