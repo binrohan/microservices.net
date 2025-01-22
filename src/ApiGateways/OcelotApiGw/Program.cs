6baa22d4abe896026927377e097b58511c8e3a63
binrohan <binrohan.cs@gmail.com>
2025-01-23 00:38:53 +0600

    [Section 9] (End of 10) Ocelot config added

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -4,6 +4,7 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging")).AddConsole().AddDebug();
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
 builder.Services.AddOcelot();
 
 var app = builder.Build();
