a411d666e8eebc89bfe7d5b6e133291a550d6968
binrohan <binrohan.cs@gmail.com>
2025-01-26 14:39:01 +0600

    [Section 10] (End) Added Aggregated patterns

--- src/ApiGateways/Shopping.Aggregator/Models/BasketItemExtendedModel.cs
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketItemExtendedModel.cs
@@ -7,7 +7,7 @@ public class BasketItemExtendedModel
     public int Quantity { get; set; }
     public string? Color { get; set; }
     public decimal Price { get; set; }
-    public string? ProductId { get; set; }
+    public required string ProductId { get; set; }
     public string? ProductName { get; set; }
 
     //Product Related Additional Fields
--- src/ApiGateways/Shopping.Aggregator/Program.cs
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -1,10 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
+                c.BaseAddress = new Uri(builder.Configuration["ApiSettings:CatalogBaseUrl"]!));
 
+builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketBaseUrl"]!));
+
+builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderBaseUrl"]!));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,25 +25,38 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
+app.MapGet("api/v1/Shopping/{userName}", async (string userName,
+                                                [FromServices] ICatalogService catalogService,
+                                                [FromServices] IBasketService basketService,
+                                                [FromServices] IOrderService orderService) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var basket = await basketService.GetBasket(userName);
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    foreach (var item in basket.Items)
+    {
+        var product = await catalogService.GetCatalog(item.ProductId);
+
+        if (product is null) continue;
+
+        item.Category = product.Category;
+        item.Summary = product.Summary;
+        item.Description = product.Description;
+        item.ImageFile = product.ImageFile;
+    }
+
+    var orders = await orderService.GetOrdersByUserName(userName);
+
+    ShoppingModel shoppingModel = new()
+    {
+        UserName = userName,
+        BasketWithProducts = basket,
+        Orders = orders
+    };
+
+    return Results.Ok(shoppingModel);
 })
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+.WithName("GetShopping")
+.Produces<ShoppingModel>(StatusCodes.Status200OK);
 
 app.Run();
 
